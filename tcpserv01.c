1548964e6fdc5324b265e84ebc9c8bb56a9d43d7
WhaleFall <280690956@qq.com>
2019-01-24 09:00:44 +0800

    修改了服务器的代码避免出现崩溃无法解决

--- tcpserv01.c
+++ tcpserv01.c
@@ -4,9 +4,10 @@ int
 main(int argc, char **argv)
 {
 	int					listenfd, connfd;
-	struct sockaddr_in	servaddr;
-	char				buff[MAXLINE];
-	time_t				ticks;
+	pid_t 				childpid;
+	socklen_t			clilen;
+	struct sockaddr_in	cliaddr,servaddr;
+	void 				sig_chld(int);
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
@@ -14,11 +15,22 @@ main(int argc, char **argv)
 	servaddr.sin_port = htons(13);
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 	Listen(listenfd, LISTENQ);
+	Signal(SIGCHLD,sig_chld);
 	for (;;) {
-		connfd = Accept(listenfd, (SA *) NULL, NULL);
-		ticks = time(NULL);
-		snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-		Write(connfd, buff, strlen(buff));
+		clilen = sizeof(cliaddr);
+		if((connfd = accept(listenfd,(SA *) &cliaddr,&clilen)) <0)
+		{
+			if(errno == EINTR)
+				continue;
+			else
+				err_sys("accept error");
+		}
+		if((childpid = Fork()) == 0)
+		{
+			Close(listenfd);
+			str_echo(connfd);
+			exit(0);
+		}
 		Close(connfd);
 	}
 }
