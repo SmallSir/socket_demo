33cde2682af860b9826483531d83c6f7ef9abfab
WhaleFall <280690956@qq.com>
2019-01-28 17:00:07 +0800

    使用select函数实现服务器关闭就会提醒用户

--- str_cli.c
+++ str_cli.c
@@ -3,11 +3,26 @@
 typedef long long LL;
 
 void str_cli(FILE *fp,int sockfd){
-    char sendline[MAXLINE],recvline[MAXLINE];
-    while(Fgets(sendline,MAXLINE,fp)!=NULL)
+    int         maxfdp1;
+    fd_set      rset;
+    char        sendline[MAXLINE],recvline[MAXLINE];
+    FD_ZERO(&rset);
+    for(;;)
     {
-        if(Readline(sockfd,recvline,MAXLINE) == 0)
-            err_quit("str_cli：server terminated prematurely");
-        Fputs(recvline,stdout);
+        FD_SET(fileno(fp),&rset);
+        FD_SET(sockfd,&rset);
+        maxfdp1 = max(fileno(fp),sockfd) + 1;
+        Select(maxfdp1,&rset,NULL,NULL,NULL);
+
+        if(FD_ISSET(sockfd,&rset)){/*socket 是可读的*/
+            if(Readline(sockfd,recvline,MAXLINE) == 0)
+                err_quit("str_cli: server terminated prematurely");
+            Fputs(recvline,stdout);
+        }
+        if(FD_ISSET(fileno(fp),&rset)){/*input 是可读的*/
+            if(Fgets(sendline,MAXLINE,fp) == NULL)
+                return 
+            Writen(sockfd,sendline,strlen(sendline));
+        }
     }
 }
