ea0ea51a8b4de0f32d996618e1122459a2fa9e59
WhaleFall <280690956@qq.com>
2019-01-28 17:21:31 +0800

    使用select获取服务器关闭时的信息，shutdown允许正确的批量输入即发送FIN

--- str_cli.c
+++ str_cli.c
@@ -3,26 +3,43 @@
 typedef long long LL;
 
 void str_cli(FILE *fp,int sockfd){
-    int         maxfdp1;
+    int         maxfdp1,stdineof;
     fd_set      rset;
-    char        sendline[MAXLINE],recvline[MAXLINE];
+    char        buf[MAXLINE];
+    int         n;
+
+    stdineof = 0;
     FD_ZERO(&rset);
     for(;;)
     {
-        FD_SET(fileno(fp),&rset);
+        if(stdineof  == 0)
+            FD_SET(fileno(fp),&rset);
         FD_SET(sockfd,&rset);
         maxfdp1 = max(fileno(fp),sockfd) + 1;
         Select(maxfdp1,&rset,NULL,NULL,NULL);
-
-        if(FD_ISSET(sockfd,&rset)){/*socket 是可读的*/
-            if(Readline(sockfd,recvline,MAXLINE) == 0)
-                err_quit("str_cli: server terminated prematurely");
-            Fputs(recvline,stdout);
+        
+        if(FD_ISSET(sockfd,&rset)){ /*socked是可读的*/
+            if((n = Read(sockfd,buf,MAXLINE)) == 0)
+            {
+                if(stdineof == 1)
+                    return;
+                else
+                {
+                    err_quit("str_cli:server terminated prematurely");
+                }
+                
+            }
+            Write(fileno(stdout),buf,n);
         }
-        if(FD_ISSET(fileno(fp),&rset)){/*input 是可读的*/
-            if(Fgets(sendline,MAXLINE,fp) == NULL)
-                return 
-            Writen(sockfd,sendline,strlen(sendline));
+        if(FD_ISSET(fileno(fp),&rset)){ /*input是可读的*/
+            if((n = Read(fileno(fp),buf,MAXLINE)) == 0)
+            {
+                stdineof = 1;
+                Shutdown(sockfd,SHUT_WR);
+                FD_CLR(fileno(fp),&rset);
+                continue;
+            }
+            Writen(sockfd,buf,n);
         }
     }
 }
