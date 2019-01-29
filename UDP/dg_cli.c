0ac2c8fffc1ee38f8884900672a557084942aff4
WhaleFall <280690956@qq.com>
2019-01-29 22:50:22 +0800

    当已知要给同一个目的地址发送多个数据报时

--- UDP/dg_cli.c
+++ UDP/dg_cli.c
@@ -5,9 +5,10 @@ void dg_cli(FILE *fp,int sockfd,const SA *pservaddr,socklen_t servlen)
 {
     int n;
     char sendline[MAXLINE],recvline[MAXLINE + 1];
+    Connect(sockfd,(SA *)pservaddr,servlen);
     while(Fgets(sendline,MAXLINE,fp)!=NULL)
     {
-        Sendto(sockfd,sendline,strlen(sendline),0,pservaddr,servlen);
+        Write(sockfd,sendline,strlen(sendline));
         n = Recvfrom(sockfd,recvline,MAXLINE,0,NULL,NULL);
         recvline[n] = 0;
         Fputs(recvline,stdout);
